5a84c945de703a86663b1c596ced8294656ec0f2
dimitar.gueorguiev <dimitar.gueorguiev@celect.comw>
2019-11-23 22:34:28 -0500

    C++ 20 examples

--- c++20-lang/compile.sh
+++ c++20-lang/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 clang++ -std=c++2a ./cpp_v20_ex_p90.cpp -o cpp_v20_ex_p90
 clang++ -std=c++2a ./cpp_v20_ex_p126.cpp -o cpp_v20_ex_p126
-g++ -std=c++2a -I/opt/gcc-latest -L/opt/gcc-latest ./cpp_v20_ex_p518.cp
-p -o cpp_v20_ex_p518
+g++ -std=c++2a -I/opt/gcc-latest -L/opt/gcc-latest ./cpp_v20_ex_p518.cpp -o cpp_v20_ex_p518
+g++ -std=c++2a -I/opt/gcc-latest -L/opt/gcc-latest ./cpp_v17_ex_hash.cpp -o cpp_v17_ex_hash
