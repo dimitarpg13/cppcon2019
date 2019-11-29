67ec6d1bbcd55d8bd1cf93ade10a1fd47272eeec
dimitarpg13 <dimitar_pg13@hotmail.com>
2019-11-29 16:14:49 -0500

    C++ 11 variadic template examples

--- c++20-lang/compile.sh
+++ c++20-lang/compile.sh
@@ -5,3 +5,4 @@ g++ -std=c++2a -I/opt/gcc-latest -L/opt/gcc-latest ./cpp_v20_ex_p518.cpp -o cpp_
 g++ -std=c++2a -I/opt/gcc-latest -L/opt/gcc-latest ./cpp_v17_ex_hash.cpp -o cpp_v17_ex_hash
 g++ -std=c++2a -I/opt/gcc-latest -L/opt/gcc-latest ./cpp_v20_ex_hashable.cpp -o cpp_v20_ex_hashable
 clang++ -std=c++11 -DDEBUG ./cpp_v11_ex_variadic_adder.cpp -o cpp_v11_ex_variadic_adder
+clang++ -std=c++11 -DDEBUG ./cpp_v11_ex_variadic_pair_comp.cpp -o cpp_v11_ex_variadic_pair_comp
