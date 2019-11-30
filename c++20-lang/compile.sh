6690cb9fa7b6375efdd6ab650686ef05991370a3
dimitarpg13 <dimitar_pg13@hotmail.com>
2019-11-30 13:24:58 -0500

    data layout of virt classes

--- c++20-lang/compile.sh
+++ c++20-lang/compile.sh
@@ -7,3 +7,4 @@ g++ -std=c++2a -I/opt/gcc-latest -L/opt/gcc-latest ./cpp_v20_ex_hashable.cpp -o 
 clang++ -std=c++11 -DDEBUG ./cpp_v11_ex_variadic_adder.cpp -o cpp_v11_ex_variadic_adder
 clang++ -std=c++11 -DDEBUG ./cpp_v11_ex_variadic_pair_comp.cpp -o cpp_v11_ex_variadic_pair_comp
 clang -cc1 -fdump-record-layouts ./cpp_v11_ex_dump_mem_layout.cpp | tee dump.txt
+clang -cc1 -fdump-record-layouts ./cpp_v11_ex1_semantics_of_data.cpp | tee dump1.txt
