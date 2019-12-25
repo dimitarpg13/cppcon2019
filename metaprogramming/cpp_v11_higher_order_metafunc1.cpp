f6a67243c1c8d9946b50563f6af3cf03e907f526
dimitar.gueorguiev <dimitar.gueorguiev@celect.comw>
2019-12-25 14:59:29 -0500

    metaprogramming book

---
+++ metaprogramming/cpp_v11_higher_order_metafunc1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <boost/type_traits/add_pointer.hpp>
+#include <boost/type_traits/is_same.hpp>
+#include <boost/static_assert.hpp>
+#include <boost/mpl/placeholders.hpp>
+
+namespace mpl = boost::mpl;
+using namespace mpl::placeholders;
+
+template <class F, class X>
+struct twice 
+{
+   typedef typename F::template apply<X>::type once ; // f(x)
+   typedef typename F::template apply<once>::type type; // f(x)
+};
+
+struct add_pointer_f
+{
+   template <class T>
+   struct apply : boost::add_pointer<T> {};
+};
+
+int main(const int argc, const char * const argv[]) {
+
+  BOOST_STATIC_ASSERT((
+     boost::is_same<
+          twice<add_pointer_f, int>::type
+        , int**
+     >::value
+  )); 
+
+  return 0;
+}
