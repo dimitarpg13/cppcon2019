18a76500666a39d38453b5d2e365e90d1fce5fef
dimitar.gueorguiev <dimitar.gueorguiev@celect.comw>
2019-12-25 22:38:00 -0500

    metaprogramming book

--- metaprogramming/compile.sh
+++ metaprogramming/compile.sh
@@ -13,3 +13,4 @@ clang++ -std=c++11 -I/opt/boost-build/include -L/opt/boost-build/lib cpp_v11_mpl
 clang++ -std=c++11 -I/opt/boost-build/include -L/opt/boost-build/lib cpp_v11_mpl_quantity_algebra_final2.cpp -o cpp_v11_mpl_quantity_algebra_final2
 clang++ -std=c++11 -I/opt/boost-build/include -L/opt/boost-build/lib cpp_v11_mpl_quantity_algebra_final3.cpp -o cpp_v11_mpl_quantity_algebra_final3
 clang++ -std=c++11 -I/opt/boost-build/include -L/opt/boost-build/lib cpp_v11_higher_order_metafunc1.cpp -o cpp_v11_higher_order_metafunc1
+clang++ -std=c++11 -I/opt/boost-build/include -L/opt/boost-build/lib cpp_v11_higher_order_metafunc2.cpp -o cpp_v11_higher_order_metafunc2
