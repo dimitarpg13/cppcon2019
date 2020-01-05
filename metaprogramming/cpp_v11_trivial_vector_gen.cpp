ef376833e11d612ba1d20e13e6d3ad0ceee68045
dimitar.gueorguiev <dimitar.gueorguiev@celect.comw>
2020-01-05 11:26:42 -0500

    metaprogramming book

--- metaprogramming/cpp_v11_trivial_vector_gen.cpp
+++ metaprogramming/cpp_v11_trivial_vector_gen.cpp
@@ -18,5 +18,9 @@ using vector_of_t = typename mpl::apply<make_vector<_>, T>::type;
 }
 
 int main(const int argc, const char * const argv[]) {
+  vector_gen_tests::vector_of_t<int> vec;
+  vec.push_back(1);
+  vec.push_back(2);
+  std::cout << "vec[0] = " << vec[0] << ", vec[1] = " << vec[1] << std::endl;
   return 0;
 }
